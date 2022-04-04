73d6b67f1607505e490cd18bfa8063a882e7404a
nmemarcoding <nmemarcoding@gmail.com>
2022-04-04 12:01:38 -0700

    fixed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,86 +2,94 @@ const { User } = require('../models');
 
 const userController = {
     // find all users
-    getAllUsers(req, res) {
+    getAllUsers(req,res) {
         User.find({})
-            .populate({
-                path: 'thoughts',
-                select: '-__v'
-            })
-            .select('-__v')
-            .then(dbUserData => res.json(dbUserData))
-            .catch(err => { res.status(400).json(err) });
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {res.status(400).json(err)});
     },
 
     // find one user by id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-            .populate({
-                path: 'thoughts',
-                select: '-__v'
-            })
-            .select('-__v')
-            .then(dbUserData => {
-                if (!dbUserData) {
-                    res.status(404).json({ message: 'No user found with this id!' });
-                    return;
-                }
-                res.json(dbUserData);
-            })
-            .catch(err => { res.status(400).json(err) });
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {res.status(400).json(err)});
     },
 
-    // create user
-    createUser({ body }, res) {
-        User.create(body)
+        // create user
+        createUser({ body }, res) {
+            User.create(body)
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.status(400).json(err));
-    },
+        },
 
-    // update User by id
-    updateUser({ params, body }, res) {
-        User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        // update User by id
+        updateUser({ params, body }, res) {
+            User.findOneAndUpdate({ _id: params.id }, body, { new: true })
             .then(dbUserData => {
-                if (!dbUserData) {
-                    res.status(404).json({ message: 'No user found with this id!' });
-                    return;
-                }
-                res.json(dbUserData);
-            })
-            .catch(err => res.status(400).json(err));
-    },
-
-    // delete User
-    deleteUser({ params }, res) {
-        User.findOneAndDelete({ _id: params.id })
+                if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+        },
+        
+        // delete User
+        deleteUser({ params }, res) {
+            User.findOneAndDelete({ _id: params.id })
             .then(dbUserData => {
                 if (!dbUserData) {
                     res.status(404).json({ message: 'No User found with this id!' });
                     return;
                 }
-                res.json({ message: 'User deleted' });
+                res.json({message: 'User deleted'});
             })
             .catch(err => res.status(400).json(err));
-    },
+        },
 
-    addFriend({ params }, res) {
-        User.findOneAndUpdate({ _id: params.userId }, { $push: { friends: params.friendId } }, { new: true, runValidators: true })
+        addFriend({ params }, res) {
+            User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { friends: params.friendId } },
+                { new: true, runValidators: true  }
+            )
             .then(dbUserData => {
                 if (!dbUserData) {
-                    res.status(404).json({ message: 'No user found with this id!' });
+                    res.status(404).json({ message: 'No user found with this id!'});
                     return;
                 }
                 res.json(dbUserData);
             })
             .catch(err => res.json(err));
-    },
-
-    // remove reaction
-    deleteFriend({ params }, res) {
-        User.findOneAndUpdate({ _id: params.userId }, { $pull: { friends: params.friendId } }, { new: true })
+        },
+    
+        // remove reaction
+        deleteFriend({ params }, res) {
+            User.findOneAndUpdate(
+                { _id: params.userId },
+                { $pull: { friends: params.friendId } },
+                { new: true }
+            )
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.json(err));
-    }
+        }
 }
 
 module.exports = userController;
--- models/Thought.js
+++ models/Thought.js
@@ -1,63 +1,57 @@
 const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ReactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        }
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
     },
-    {
-        toJSON: {
-            getters: true
-        },
-        id: false
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
     }
-)
+}, {
+    toJSON: {
+        getters: true
+    },
+    id: false
+})
 
 
-const ThoughtSchema = new Schema(
-    {  
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [ReactionSchema]
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
     },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
-);
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
 
 ThoughtSchema.virtual('ractionCount').get(function() {
     return this.reactions.length;
@@ -65,4 +59,4 @@ ThoughtSchema.virtual('ractionCount').get(function() {
 
 const Thought = model('Thought', ThoughtSchema);
 
-module.exports = Thought ; 
+module.exports = Thought;
